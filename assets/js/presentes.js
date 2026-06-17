4f2dd8273f8b5770be8e42b876836dcfaf32283e
WandrewRicardo <wandrewricardo@gmail.com>
2026-06-17 17:57:24 -0300

    Aplicando novo visual no menu hamburguer da pagina de presentes

--- assets/js/presentes.js
+++ assets/js/presentes.js
@@ -1,9 +1,15 @@
 function menuHamburguer() {
     const btnMenu = document.querySelector('.btn-menu');
     const navItems = document.querySelector('.nav-items');
+    const btnClose = document.querySelector('.btn-close');
 
     btnMenu.addEventListener('click', () => {
         navItems.classList.toggle('active')
+    });
+
+     btnClose.addEventListener('click', ()=>{
+        navItems.classList.remove('active');
+        btnClose.classList.remove('active');
     });
 }
 menuHamburguer();
