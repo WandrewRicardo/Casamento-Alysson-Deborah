06dd23551d9c081dab77641cb12373be0cb16ee2
WandrewRicardo <wandrewricardo@gmail.com>
2026-06-26 18:09:15 -0300

    pequena correção em Controller

--- backend/src/controllers/rsvpController.js
+++ backend/src/controllers/rsvpController.js
@@ -1,5 +1,14 @@
 const rsvpService = require('../services/rsvpService.js')
-const express = require('express')
-const Router = express.Router
+
+async function rsvpController(req, res) {
+
+const { nome, numero_convite } = req.body
+
+    const resultado = await rsvpService.confirmarPresenca(nome, numero_convite)
+
+    return res.status(resultado.status).json({
+        mensagem: resultado.mensagem
+    })
+}
 
 module.exports = rsvpController
